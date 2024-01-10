5d572c7d408f47b95bb5e187625a4581fa71cb1c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-10 11:42:35 -0300

    refactor: add projects

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -52,6 +52,16 @@ export const RESUME_DATA = {
   ],
   projects: [
     {
+      title: "riannegreiros.dev",
+      techStack: ["Next.js", "ASP.NET Core", "PostgreSQL", "Redis", "MongoDB"],
+      description:
+        "Meu site e blog pessoal. Criado com Next.js e ASP.NET Core",
+      link: {
+        label: "github.com/RianNegreiros/portfolio",
+        href: "https://riannegreiros.dev",
+      },
+    },
+    {
       title: "Sistema de rastreamento de veículos",
       techStack: ["ASP.NET Core", "Golang", "Next.js", "Redis", "MongoDB", "Kafka"],
       description:
@@ -65,7 +75,7 @@ export const RESUME_DATA = {
       title: "SpotCode",
       techStack: ["Spring", "Oauth2", "H2"],
       description:
-        "Uma API de música inspirada no Spotify",
+        "Uma API de música desenvolvida usando o Spring e Java, inspirada no Spotify",
       link: {
         label: "https://github.com/RianNegreiros/SpotCode",
         href: ""
@@ -75,7 +85,7 @@ export const RESUME_DATA = {
       title: "EnterpriseStock",
       techStack: ["Spring", "Oauth2", "PostgreSQL", "JUnit5"],
       description:
-        "API RESTful para catalogação de produtos com foco na autorização e autenticação",
+        "API RESTful desenvolvida usando o Spring e Java para catalogação de produtos feito com foco na autorização e autenticação",
       link: {
         label: "https://github.com/RianNegreiros/EnterpriseStock",
         href: ""
@@ -89,6 +99,36 @@ export const RESUME_DATA = {
       link: {
         label: "https://github.com/RianNegreiros/LearnifyAPI",
         href: ""
+      },
+    },
+    {
+      title: "URL Shortener",
+      techStack: ["Golang", "Redis", "Fiber"],
+      description:
+        "Serviço que permite encurtar URLs longos para torná-los mais compactos e fáceis de compartilhar",
+      link: {
+        label: "https://github.com/RianNegreiros/url-shortener-go-redis",
+        href: ""
+      },
+    },
+    {
+      title: "Kafka Clone",
+      techStack: ["Golang"],
+      description:
+        "Uma implementação simplificada de um sistema de corretor de mensagens, inspirado no Apache Kafka",
+      link: {
+        label: "https://github.com/RianNegreiros/devgym-kafka-clone",
+        href: ""
+      },
+    },
+    {
+      title: "Vigilate",
+      techStack: ["Golang", "Next.js", "PostgreSQL", "Apache Kafka"],
+      description:
+        "Health Checker de serviços Web com monitoramento em tempo real usando Pusher e Apache Kafka",
+      link: {
+        label: "https://github.com/RianNegreiros/vigilate",
+        href: "https://vigilate.vercel.app",
       },
     },
   ],
